6baaa22d3ae0f790b2d7992995d6876032258e94
Bobby S <136953930+Bc5Neep@users.noreply.github.com>
2023-08-11 22:35:24 -0700

    Robert Sims edit of lexical scoping assignment

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,19 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) { ## Create matrix capable of caching its inverse
+  inv<- NULL
+  set <- function(z){
+    x <<- z
+    inv<<- NULL
+  }
+  
+  get <- function()x                ## Retrieves matrix x
+  setinverse <- function(inverse)inv<<- inverse
+  getinverse <- function()inv       ## Function to get inverse of matrix x
+  list(set=set, get=get, 
+       setinverse=setinverse,
+       getinverse=getinverse)
 }
 
 
@@ -12,4 +24,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x,...){ ## Get cache data
+  inv <-x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached result")
+    return (inv)
+  }
+  data <-x$get()
+  inv <-solve(data,...)      ## Calculate inverse values of matrix
+  x$setinverse(inv)
+  inv                        ## Returns matrix that is the inverse of x
 }
